a5e5c4cad99461dd4dd79241ac7a173b3f5e1a98
Gudmundur Einarsson <gumeo140688@gmail.com>
2017-12-13 11:08:24 +0100

    small fix

--- R/downloadAndLoad.R
+++ R/downloadAndLoad.R
@@ -39,10 +39,10 @@ download_mnist <- function(where = "./"){
   download.file('http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
                 destfile = testLabs)
   # Unzip the downloaded files
-  gunzip(trainImgs)
-  gunzip(trainLabs)
-  gunzip(testImgs)
-  gunzip(testLabs)
+  unzip(trainImgs)
+  unzip(trainLabs)
+  unzip(testImgs)
+  unzip(testLabs)
   print(paste('Mnist dataset has been downloaded to: ', where))
   return(TRUE)
 }
