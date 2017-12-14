cc2af4ae34a46057c1fc0a0814bcd47b9aa87770
Gudmundur Einarsson <gumeo140688@gmail.com>
2017-12-14 11:31:17 +0100

    Just making sure that ggplot2 functions are called

--- R/basicPlotting.R
+++ R/basicPlotting.R
@@ -8,7 +8,8 @@
 #' title with the digit label and a predicted label.
 #'
 #' @param digit A single row from the mnist dataset, i.e. a
-#'        1 by 784 vector. This can e.g. be \code{trainSet$x[1,]}.
+#'        1 by 784 vector. This can e.g. be the first digit from the
+#'        mnist dataset \code{trainSet$x[1,]}.
 #' @param trueLab The true label of the digit. Can be a factor or character.
 #' @param predLab The predicted label, e.g. from a classifier.
 #'        Can be a factor or character.
@@ -28,23 +29,23 @@
 #' @export
 ggDigit <- function(digit, trueLab, predLab){
   pDig <- data.frame(value = as.numeric(t(digit)), xVar = rep(1:28,28), yVar = -rep(1:28,each=28)+28)
-  ggRet <- ggplot(data = pDig, aes(x=xVar, y=yVar, fill=value)) +
+  ggRet <- ggplot2::ggplot(data = pDig, aes(x=xVar, y=yVar, fill=value)) +
     geom_tile(color = "white")+
-    scale_fill_gradient2(low = "white", high = "black", mid = "gray",
+    ggplot2::cale_fill_gradient2(low = "white", high = "black", mid = "gray",
                          midpoint = max(pDig$value)/2, limit = c(min(pDig$value),max(pDig$value)),
                          space = "Lab", guide = FALSE) +
-    theme_minimal()+
-    coord_fixed() + xlab("") + ylab("") +
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    ggplot2::theme_minimal()+
+    ggplot2::coord_fixed() + ggplot2::xlab("") + ggplot2::ylab("") +
+    ggplot2::theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
           axis.text.x = element_blank(),axis.text.y = element_blank())
   if(missing(trueLab) & missing(predLab)){
     return(ggRet)
   }else if(missing(predLab)){
-    ggRet <- ggRet + ggtitle(paste('True label is:',as.character(trueLab)))
+    ggRet <- ggRet + ggplot2::ggtitle(paste('True label is:',as.character(trueLab)))
   }else if(missing(trueLab)){
-    ggRet <- ggRet + ggtitle(paste('Predicted label is:',as.character(predLab)))
+    ggRet <- ggRet + ggplot2::ggtitle(paste('Predicted label is:',as.character(predLab)))
   }else{
-    ggRet <- ggRet + ggtitle(paste('True label:',as.character(trueLab),
+    ggRet <- ggRet + ggplot2::ggtitle(paste('True label:',as.character(trueLab),
                                    ", Predicted label:", as.character(predLab)))
   }
   return(ggRet)
